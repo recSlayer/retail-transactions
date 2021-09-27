d5b54b63e81bc02d60a1d2e42483d925254d9ea1
Paul Cantrell <cantrell@pobox.com>
2021-09-27 18:43:58 -0500

    Better test reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,47 @@
-# Require everything directly under lib/
+# encoding: UTF-8
 
+# Require everything directly under lib/
 Dir[File.join(File.dirname(__FILE__), "../lib/*")].each do |lib_file|
   require lib_file
 end
 
 require "minitest/autorun"
+require "minitest/reporters"
 
-# To see full test names when running tests:
-#
-# require "minitest/reporters"
-# Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+class CustomReporter < Minitest::Reporters::DefaultReporter
+  def before_suite(suite)
+  end
+
+  def on_record(result)
+    super
+
+    test_path = result.klass.split("::")
+    test_path << result.name.gsub(/^test_\d+_/, '')
+    print " "
+    color = if result.skipped?
+      method(:yellow)
+    elsif result.failure
+      method(:red)
+    else
+      :itself
+    end
+    puts test_path.map(&color).join(blue(" · "))
+  end
+
+  def record_pass(record)
+    green("✔︎")
+  end
+
+  def record_skip(record)
+    yellow("S")
+  end
+
+  def record_failure(record)
+    red("✖︎")
+  end
+end
+
+Minitest::Reporters.use! CustomReporter.new
 
 module Minitest::Assertions
   def assert_invalid_transition
